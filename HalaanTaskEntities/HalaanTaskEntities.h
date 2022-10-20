07d696156aadccb8f00714bc812b74880aca0a38
Mohamed El-Taweel <mohamedmagdy.9080@gmail.com>
2022-10-20 13:46:35 +0200

    Initial Commit

---
+++ HalaanTaskEntities/HalaanTaskEntities.h
@@ -0,0 +1,18 @@
+//
+//  HalaanTaskEntities.h
+//  HalaanTaskEntities
+//
+//  Created by Mohamed on 20/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HalaanTaskEntities.
+FOUNDATION_EXPORT double HalaanTaskEntitiesVersionNumber;
+
+//! Project version string for HalaanTaskEntities.
+FOUNDATION_EXPORT const unsigned char HalaanTaskEntitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HalaanTaskEntities/PublicHeader.h>
+
+
